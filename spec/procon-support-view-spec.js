baf668cbb79c2838e06eda0e9335d7b2f0724df3
naoya_t <naoya.tozuka@gmail.com>
2018-08-09 13:18:24 +0900

    procon support

---
+++ spec/procon-support-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ProconSupportView from '../lib/procon-support-view';
+
+describe('ProconSupportView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
